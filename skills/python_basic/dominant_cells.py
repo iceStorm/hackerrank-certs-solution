8b7b082d98548ec4854d60e1f04997aea5a2c628
iceStorm <hauduedaiduong@gmail.com>
2023-11-16 14:10:00 +0700

    python basic

--- skills/python_basic/dominant_cells.py
+++ skills/python_basic/dominant_cells.py
@@ -1,18 +1,3 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-
-#
-# Complete the 'numCells' function below.
-#
-# The function is expected to return an INTEGER.
-# The function accepts 2D_INTEGER_ARRAY grid as parameter.
-#
 def numCells(grid):
     # Write your code here
     result = 0
@@ -34,21 +19,3 @@ def numCells(grid):
                 result += 1
 
     return result
-
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    grid_rows = int(input().strip())
-    grid_columns = int(input().strip())
-
-    grid = []
-
-    for _ in range(grid_rows):
-        grid.append(list(map(int, input().rstrip().split())))
-
-    result = numCells(grid)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
--- skills/python_basic/reverse_words_and_swap_cases.py
+++ skills/python_basic/reverse_words_and_swap_cases.py
@@ -1,18 +1,3 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-
-#
-# Complete the 'reverse_words_order_and_swap_cases' function below.
-#
-# The function is expected to return a STRING.
-# The function accepts STRING sentence as parameter.
-#
 
 def reverse_words_order_and_swap_cases(sentence):
     words = sentence.split(' ')
@@ -31,15 +16,3 @@ def reverse_words_order_and_swap_cases(sentence):
     print('final:', final_sentence)
 
     return final_sentence.strip()
-
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    sentence = input()
-
-    result = reverse_words_order_and_swap_cases(sentence)
-
-    fptr.write(result + '\n')
-
-    fptr.close()
