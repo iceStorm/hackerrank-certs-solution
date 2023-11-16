561045610eab086793aa9f5e2253f44622601306
iceStorm <hauduedaiduong@gmail.com>
2023-11-16 16:53:26 +0700

    software engineer intern

--- roles/Software Engineer Intern Role Certification Test/growth_in_2_dimensions.js
+++ roles/Software Engineer Intern Role Certification Test/growth_in_2_dimensions.js
@@ -1,58 +1,18 @@
-"use strict";
-
-const fs = require("fs");
-
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-
-let inputString = "";
-let currentLine = 0;
-
-process.stdin.on("data", function (inputStdin) {
-  inputString += inputStdin;
-});
-
-process.stdin.on("end", function () {
-  inputString = inputString.split("\n");
-
-  main();
-});
-
-function readLine() {
-  return inputString[currentLine++];
-}
-
-/*
- * Complete the 'countMax' function below.
- *
- * The function is expected to return a LONG_INTEGER.
- * The function accepts STRING_ARRAY upRight as parameter.
- */
-
-/**
- * @param {Array<string>} upRight
- */
 function countMax(upRight) {
-  upRight.forEach((coordinates) => {
-    const [row, column] = coordinates.split(" ").map((item) => parseInt(item));
-  });
-}
+  let xl = 1;
+  let yl = 1;
 
-function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  xl = parseInt(upRight[1].split(" ")[0]);
+  yl = parseInt(upRight[1].split(" ")[1]);
 
-  const upRightCount = parseInt(readLine().trim(), 10);
-
-  let upRight = [];
-
-  for (let i = 0; i < upRightCount; i++) {
-    const upRightItem = readLine();
-    upRight.push(upRightItem);
+  for (let i = 0; i < upRight.length; i++) {
+    if (xl > parseInt(upRight[i].split(" ")[0])) {
+      xl = parseInt(upRight[i].split(" ")[0]);
+    }
+    if (yl > parseInt(upRight[i].split(" ")[1])) {
+      yl = parseInt(upRight[i].split(" ")[1]);
+    }
   }
 
-  const result = countMax(upRight);
-
-  ws.write(result + "\n");
-
-  ws.end();
+  return yl * xl;
 }
