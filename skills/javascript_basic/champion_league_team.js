edc6a3deb170cdf904b7f0afc296a6e3e50c3cde
iceStorm <hauduedaiduong@gmail.com>
2023-11-17 12:00:04 +0700

    js basic

--- skills/javascript_basic/champion_league_team.js
+++ skills/javascript_basic/champion_league_team.js
@@ -1,28 +1,3 @@
-"use strict";
-
-const fs = require("fs");
-const https = require("https");
-
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-
-let inputString = "";
-let currentLine = 0;
-
-process.stdin.on("data", function (inputStdin) {
-  inputString += inputStdin;
-});
-
-process.stdin.on("end", function () {
-  inputString = inputString.split("\n");
-
-  main();
-});
-
-function readLine() {
-  return inputString[currentLine++];
-}
-
 const axios = require("axios");
 
 async function getTeams(year, k) {
@@ -71,17 +46,4 @@ async function getMatches(year, k, page = 1, teams = {}) {
   }
 
   return teams;
-}
-
-async function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-  const year = parseInt(readLine().trim());
-  const k = parseInt(readLine().trim());
-
-  const teams = await getTeams(year, k);
-
-  for (const team of teams) {
-    ws.write(`${team}\n`);
-  }
 }
