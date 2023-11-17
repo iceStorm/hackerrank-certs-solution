21c67647c46a8c0175f74f5c6404a16c447ab73a
iceStorm <hauduedaiduong@gmail.com>
2023-11-17 12:34:57 +0700

    problem solving basic

--- skills/problem_solving_basic/maximum_cost_laptop_count.js
+++ skills/problem_solving_basic/maximum_cost_laptop_count.js
@@ -1,37 +1,3 @@
-"use strict";
-
-const fs = require("fs");
-
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-
-let inputString = "";
-let currentLine = 0;
-
-process.stdin.on("data", function (inputStdin) {
-  inputString += inputStdin;
-});
-
-process.stdin.on("end", function () {
-  inputString = inputString.split("\n");
-
-  main();
-});
-
-function readLine() {
-  return inputString[currentLine++];
-}
-
-/*
- * Complete the 'maxCost' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts following parameters:
- *  1. INTEGER_ARRAY cost
- *  2. STRING_ARRAY labels
- *  3. INTEGER dailyCount
- */
-
 /**
  * @param {number[]} cost
  * @param {string[]} labels
@@ -61,34 +27,4 @@ function maxCost(cost, labels, dailyCount) {
   }
 
   return maximumCost;
-}
-
-function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-  const costCount = parseInt(readLine().trim(), 10);
-
-  let cost = [];
-
-  for (let i = 0; i < costCount; i++) {
-    const costItem = parseInt(readLine().trim(), 10);
-    cost.push(costItem);
-  }
-
-  const labelsCount = parseInt(readLine().trim(), 10);
-
-  let labels = [];
-
-  for (let i = 0; i < labelsCount; i++) {
-    const labelsItem = readLine();
-    labels.push(labelsItem);
-  }
-
-  const dailyCount = parseInt(readLine().trim(), 10);
-
-  const result = maxCost(cost, labels, dailyCount);
-
-  ws.write(result + "\n");
-
-  ws.end();
 }
