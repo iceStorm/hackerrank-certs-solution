e4d02bbcb4e99a895df4fb7b6a3179245315aa05
iceStorm <hauduedaiduong@gmail.com>
2023-11-17 13:39:51 +0700

    software engineer intern

--- roles/Software Engineer Role Certification Test/js__maximizing_the_final_element.js
+++ roles/Software Engineer Role Certification Test/js__maximizing_the_final_element.js
@@ -1,34 +1,3 @@
-"use strict";
-
-const fs = require("fs");
-
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-
-let inputString = "";
-let currentLine = 0;
-
-process.stdin.on("data", function (inputStdin) {
-  inputString += inputStdin;
-});
-
-process.stdin.on("end", function () {
-  inputString = inputString.split("\n");
-
-  main();
-});
-
-function readLine() {
-  return inputString[currentLine++];
-}
-
-/*
- * Complete the 'getMaxValue' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts INTEGER_ARRAY arr as parameter.
- */
-
 function getMaxValue(arr) {
   console.log("original arr:", arr);
 
@@ -44,23 +13,4 @@ function getMaxValue(arr) {
   console.log("subtracted arr:", arr);
 
   return arr.pop();
-}
-
-function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-  const arrCount = parseInt(readLine().trim(), 10);
-
-  let arr = [];
-
-  for (let i = 0; i < arrCount; i++) {
-    const arrItem = parseInt(readLine().trim(), 10);
-    arr.push(arrItem);
-  }
-
-  const result = getMaxValue(arr);
-
-  ws.write(result + "\n");
-
-  ws.end();
 }
