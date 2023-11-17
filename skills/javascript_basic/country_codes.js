ae90b7ade309da8ebb1f69cc2fc4d65b945944d0
iceStorm <hauduedaiduong@gmail.com>
2023-11-17 18:23:59 +0700

    js basic

--- skills/javascript_basic/country_codes.js
+++ skills/javascript_basic/country_codes.js
@@ -1,28 +1,3 @@
-"use strict";
-
-const fs = require("fs");
-const https = require("https");
-
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-
-let inputString = "";
-let currentLine = 0;
-
-process.stdin.on("data", function (inputStdin) {
-  inputString += inputStdin;
-});
-
-process.stdin.on("end", function () {
-  inputString = inputString.split("\n");
-
-  main();
-});
-
-function readLine() {
-  return inputString[currentLine++];
-}
-
 const axios = require("axios");
 
 async function getCountryName(code) {
@@ -49,14 +24,4 @@ async function getCountry(code, pageIndex = 1) {
   }
 
   return getCountry(code, pageIndex + 1);
-}
-
-async function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-  const code = readLine().trim();
-
-  const name = await getCountryName(code);
-
-  ws.write(`${name}\n`);
 }
