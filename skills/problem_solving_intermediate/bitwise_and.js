663816d5cd9f97af7f9ab8dbd6d57a2a9f17dd51
iceStorm <hauduedaiduong@gmail.com>
2023-11-17 13:02:02 +0700

    problem solving intermediate

--- skills/problem_solving_intermediate/bitwise_and.js
+++ skills/problem_solving_intermediate/bitwise_and.js
@@ -1,36 +1,3 @@
-"use strict";
-
-const fs = require("fs");
-
-process.stdin.resume();
-process.stdin.setEncoding("utf-8");
-
-let inputString = "";
-let currentLine = 0;
-
-process.stdin.on("data", function (inputStdin) {
-  inputString += inputStdin;
-});
-
-process.stdin.on("end", function () {
-  inputString = inputString.split("\n");
-
-  main();
-});
-
-function readLine() {
-  return inputString[currentLine++];
-}
-
-/*
- * Complete the 'countPairs' function below.
- *
- * The function is expected to return a LONG_INTEGER.
- * The function accepts INTEGER_ARRAY arr as parameter.
- */
-
-// https://github.com/kilian-hu/hackerrank-solutions/blob/master/certificates/problem-solving-intermediate/bitwise-and/solution.py
-
 /**
  * @param {number[]} arr
  */
@@ -62,23 +29,4 @@ function countPairs(arr) {
   }
 
   return ans;
-}
-
-function main() {
-  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-  const arrCount = parseInt(readLine().trim(), 10);
-
-  let arr = [];
-
-  for (let i = 0; i < arrCount; i++) {
-    const arrItem = parseInt(readLine().trim(), 10);
-    arr.push(arrItem);
-  }
-
-  const result = countPairs(arr);
-
-  ws.write(result + "\n");
-
-  ws.end();
 }
