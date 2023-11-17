586a4ba4869eab1a0411a6f344b17189e689c223
iceStorm <hauduedaiduong@gmail.com>
2023-11-17 12:24:34 +0700

    python basic

--- skills/python_basic/multi_set_implementation.py
+++ skills/python_basic/multi_set_implementation.py
@@ -1,12 +1,3 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-
 class Multiset:
     def __init__(self):
         self.items = []
@@ -31,34 +22,3 @@ class Multiset:
     def __len__(self):
         # returns the number of elements in the multiset
         return len(self.items)
-
-
-if __name__ == '__main__':
-    def performOperations(operations):
-        m = Multiset()
-        result = []
-        for op_str in operations:
-            elems = op_str.split()
-            if elems[0] == 'size':
-                result.append(len(m))
-            else:
-                op, val = elems[0], int(elems[1])
-                if op == 'query':
-                    result.append(val in m)
-                elif op == 'add':
-                    m.add(val)
-                elif op == 'remove':
-                    m.remove(val)
-        return result
-
-    q = int(input())
-    operations = []
-    for _ in range(q):
-        operations.append(input())
-
-    result = performOperations(operations)
-
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-    fptr.write('\n'.join(map(str, result)))
-    fptr.write('\n')
-    fptr.close()
--- skills/python_basic/shape_classes_and_area_method.py
+++ skills/python_basic/shape_classes_and_area_method.py
@@ -1,11 +1,3 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
 
 class Rectangle:
     def __init__(self, width: float, height: float):
@@ -22,22 +14,3 @@ class Circle:
 
     def area(self):
         return math.pi * (self.radius ** 2)
-
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-    q = int(input())
-    queries = []
-    for _ in range(q):
-        args = input().split()
-        shape_name, params = args[0], tuple(map(int, args[1:]))
-        if shape_name == "rectangle":
-            a, b = params[0], params[1]
-            shape = Rectangle(a, b)
-        elif shape_name == "circle":
-            r = params[0]
-            shape = Circle(r)
-        else:
-            raise ValueError("invalid shape type")
-        fptr.write("%.2f\n" % shape.area())
-    fptr.close()
