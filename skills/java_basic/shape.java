4f422137315a658da7f82faafee9a47d5d349a99
iceStorm <hauduedaiduong@gmail.com>
2023-11-18 09:56:49 +0700

    java basic

--- skills/java_basic/shape.java
+++ skills/java_basic/shape.java
@@ -1,3 +1,8 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
 
 class Shape {
     public int length , breadth ;
@@ -17,5 +22,19 @@ class Rectangle extends Shape {
     @Override
     public void area() {
         System.out.print("\n" + length*breadth);
+    }
+}
+
+public class Solution {
+    public static void main(String args[] ) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int l = sc.nextInt();
+        int b = sc.nextInt();
+
+        Shape shape = new Shape(l,b);
+        shape.area();
+
+        Shape rectangle = new Rectangle(l,b);
+        rectangle.area();
     }
 }
